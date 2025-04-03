e3d53295bbb2621d591cb727eb0e98c1bf34dd03
htlbhagat <htlbhagat@yahoo.co.in>
2025-04-03 21:26:10 +1300

    UPDATED PROJECT FILES.

---
+++ Person Example Demo/EmployeeRoles.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Example_Demo
+{
+
+    internal class EmployeeRoles
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("THIS IS EMPLOYEE ROLES CLASS");
+
+        }
+    }
+}
